52d6070623f72a6a9d595e00a5e89fe31a44b376
kharrazmohammed <kharraz.mohammed@hotmail.com>
2025-04-25 22:11:06 +0100

    knsayb daba fdik activity lifiha dok 4 buttons

--- app/src/main/java/com/example/mathprofs/activities/MainActivity.kt
+++ app/src/main/java/com/example/mathprofs/activities/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.mathprofs.activities
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
 import com.example.mathprofs.R
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnAPropos = findViewById<Button>(R.id.btnAPropos)
+        val btnGeogebra = findViewById<Button>(R.id.btnGeogebra)
+        val btnSeries = findViewById<Button>(R.id.btnSeries)
+        val btnFiches = findViewById<Button>(R.id.btnFiches)
+
+
+        btnAPropos.setOnClickListener {
+            val intent=Intent(this,about::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
