447104703780b96011df7aa33ebc36b3e826a6e8
kharrazmohammed <kharraz.mohammed@hotmail.com>
2025-04-25 18:27:03 +0100

    about activity UI

---
+++ app/src/main/java/com/example/mathprofs/activities/about.kt
@@ -0,0 +1,12 @@
+package com.example.mathprofs.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mathprofs.R
+
+class about : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
