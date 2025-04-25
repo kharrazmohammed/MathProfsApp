871a82cb8e8618ad97a78b9c776c824e137757cd
kharrazmohammed <kharraz.mohammed@hotmail.com>
2025-04-25 21:33:39 +0100

    test

---
+++ app/src/main/java/com/example/mathprofs/activities/exercicesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mathprofs.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mathprofs.R
+
+class exercicesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercices)
+    }
+}
