316adda98e5ec213a03a39647f0627a803788ee5
kharrazmohammed <kharraz.mohammed@hotmail.com>
2025-04-25 16:18:47 +0100

    splashscreen code

--- app/src/main/java/com/example/mathprofs/activities/Splashscreen.kt
+++ app/src/main/java/com/example/mathprofs/activities/Splashscreen.kt
@@ -1,13 +1,22 @@
 package com.example.mathprofs.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import com.example.mathprofs.R
+import java.util.logging.Handler
 
 class Splashscreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
