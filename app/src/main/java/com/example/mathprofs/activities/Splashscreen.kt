d44218d0525c6297e37b7579c5871e0c4ec4de54
kharrazmohammed <kharraz.mohammed@hotmail.com>
2025-04-25 16:13:33 +0100

    test

---
+++ app/src/main/java/com/example/mathprofs/activities/Splashscreen.kt
@@ -0,0 +1,13 @@
+package com.example.mathprofs.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mathprofs.R
+
+class Splashscreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
